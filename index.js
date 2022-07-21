9b5070c942ffc23561953d3ea59733f1b0a2bce9
emersonteles <emersonteles21@gmail.com>
2022-07-21 14:58:56 -0300

    new changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function funcao() {
   console.log("f*ck mds");
+  console.log("changing index");
 }
--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
 function routes() {
   console.log("listening port 8080...");
+  console.log("changing routes");
 }
